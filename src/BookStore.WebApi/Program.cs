6b16837755dd2a8816e6df0bce17aab487276ebb
Carlos Pons <karlospn@gmail.com>
2022-11-20 19:21:38 +0100

    Add container properties on csproj

--- src/BookStore.WebApi/Program.cs
+++ src/BookStore.WebApi/Program.cs
@@ -6,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddTransient<IEmailService, EmailService>();
+
 builder.Services.AddSwaggerGen(opts =>
 {
     opts.SwaggerDoc("v1", new OpenApiInfo()
